fa705d36d4c6fd6ce94c455cfae76fb7011a8fa0
chenxi.li <1606707834@qq.com>
2021-06-15 11:34:33 +0800

    chore: 添加ts类型定义

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,26 +1,29 @@
 import { useEffect, useState } from "react";
 
-export const isFalsy = (value) => (value === 0 ? false : !value);
+export const isFalsy = (value: any) => (value === 0 ? false : !value);
 
-export const cleanObject = (object) => {
+export const cleanObject = (object: object) => {
   const result = { ...object };
   Object.keys(result).forEach((key) => {
+    // @ts-ignore
     const value = result[key];
     if (isFalsy(value)) {
+      // @ts-ignore
       delete result[key];
     }
   });
   return result;
 };
 
-export const useMount = (callback) => {
+export const useMount = (callback: () => void) => {
   // use开头是hook
   useEffect(() => {
     callback();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 };
 
-export const useDebounce = (value, delay) => {
+export const useDebounce = (value: any, delay?: number) => {
   //需要使用hook时需要写成hook
   const [debouncedValue, setDebouncedValue] = useState(value);
   useEffect(() => {
@@ -32,14 +35,14 @@ export const useDebounce = (value, delay) => {
   return debouncedValue;
 };
 
-export const debounce = (func, delay) => {
-  let timeout;
-  return (...param) => {
-    if (timeout) {
-      clearTimeout(timeout);
-    }
-    timeout = setTimeout(function () {
-      func(...param);
-    }, delay);
-  };
-};
+// export const debounce = (func, delay) => {
+//   let timeout;
+//   return (...param) => {
+//     if (timeout) {
+//       clearTimeout(timeout);
+//     }
+//     timeout = setTimeout(function () {
+//       func(...param);
+//     }, delay);
+//   };
+// };
